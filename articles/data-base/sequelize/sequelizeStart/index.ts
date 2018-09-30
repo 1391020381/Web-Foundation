314e368419db1a0b19d2b77c77ac2c8f9c62c8f6
zhoujin <zhoujin@iboxpay.com>
2018-09-30 10:52:18 +0800

    update

---
+++ articles/data-base/sequelize/sequelizeStart/index.ts
@@ -0,0 +1,21 @@
+ import Sequelize from 'sequelize'
+import { AssertionError } from 'assert';
+const sequelize = new Sequelize('nodelover','root','',{
+    host:'localhost',
+    dialect:'mysql',
+    pool:{
+        max:5,
+        min:0,
+        idle:1000 // 连接在释放之前可以空闲的最长时间(以毫秒为单位)
+    }
+})
+async function main() {
+    try{
+        await sequelize.authenticate()
+        console.log('sequelize已经连接成功啦！')
+        process.exit(0)
+    }catch(e){
+        console.log(e)
+    }
+}
+main()
