2a69a042dec34d8938eef639cfd2089690ac0f9b
行云流水justdoit <justdoit>
2018-10-07 10:25:00 +0800

    update

--- articles/data-base/sequelize/chapter1/index.ts
+++ articles/data-base/sequelize/chapter1/index.ts
@@ -1,20 +1,21 @@
- import Sequelize from 'sequelize'
+import Sequelize from 'sequelize';
 const sequelize = new Sequelize('nodelover','root','',{
     host:'localhost',
     dialect:'mysql',
     pool:{
         max:5,
         min:0,
-        idle:1000 // 连接在释放之前可以空闲的最长时间(以毫秒为单位)
+        idle:10000
     }
-})
+});
+
 async function main() {
     try{
         await sequelize.authenticate()
-        console.log('sequelize已经连接成功啦！')
+        console.log('sequelize 已经连接成功啦！')
         process.exit(0)
     }catch(e){
-        console.log(e)
+     console.log(e)
     }
-}
+};
 main()
