5a792d515dcb2d0cdc9e046191f9198a9a69a72c
zhoujin <zhoujin@iboxpay.com>
2018-10-09 14:51:54 +0800

    update

--- articles/data-base/sequelize/chapter1/index.ts
+++ articles/data-base/sequelize/chapter1/index.ts
@@ -1,21 +1,21 @@
 import Sequelize from 'sequelize';
-const sequelize = new Sequelize('nodelover','root','',{
-    host:'localhost',
-    dialect:'mysql',
-    pool:{
-        max:5,
-        min:0,
-        idle:10000
+const sequelize = new Sequelize('nodelover', 'root', 'GHzXlxXYg9&G' || 'justdoit', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
     }
 });
 
 async function main() {
-    try{
+    try {
         await sequelize.authenticate()
         console.log('sequelize 已经连接成功啦！')
         process.exit(0)
-    }catch(e){
-     console.log(e)
+    } catch (e) {
+        console.log(e)
     }
 };
 main()
