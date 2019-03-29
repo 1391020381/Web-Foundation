b39eee75c0dac33188c77abd5efff041ce8ca039
zhoujin <zhoujin@iboxpay.com>
2019-03-29 18:02:58 +0800

    tree

---
+++ articles/Front-end-frame/Vue.js 组件精讲/demo/vue-component/src/components/tree/node.vue
@@ -0,0 +1,20 @@
+<!-- node -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {},
+  computed: {},
+  mounted () { },
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ articles/Front-end-frame/Vue.js 组件精讲/demo/vue-component/src/components/tree/tree.vue
@@ -0,0 +1,50 @@
+<!-- tree 用于接受和处理数据,并将数据传递给 node.vue-->
+<template>
+  <div>
+    <tree-node v-for="(item,index) in cloneData"
+               :key="index"
+               :data="item"
+               :show-checkbox="showCheckbox">
+    </tree-node>
+  </div>
+</template>
+
+<script>
+import TreeNode from './node.vue'
+import { deepCopy } from '../../utils/assist.js'
+export default {
+  name: 'Tree',
+  data () {
+    return {
+    }
+  },
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    showCheckbox: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: { TreeNode },
+  computed: {},
+  mounted () { },
+  methods: {
+    rebuildData () {
+      this.cloneData = deepCopy(this.data)
+    }
+  },
+  watch: {
+    data () {
+      this.rebuildData()
+    }
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
